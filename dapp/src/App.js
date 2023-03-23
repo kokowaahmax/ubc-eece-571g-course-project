83feab93a761d1f73bd8d017c3262cd9f2278d3d
Xin Wang <ge49moy@mytum.de>
2023-03-23 16:19:29 -0700

    add solidity v1.0

--- dapp/src/App.js
+++ dapp/src/App.js
@@ -1,8 +1,17 @@
+// node requirements
 import logo from './logo.svg';
 import './App.css';
 import React, { useState } from "react";
+import { ethers } from 'ethers';
+
+// import ABI code to interact with the smart contract
+import contract from './artifacts/contracts/Lock.sol/Lock.json'
+
+// react component
 import MessageList from "./components/MessageList"
 import MessageForm from "./components/MessageForm"
+
+// const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
 
 function App() {
 

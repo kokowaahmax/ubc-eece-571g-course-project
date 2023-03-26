37d5934610b44ccf1ce834a7d857b34dcfbfa209
Mingrui Li <118086827+Davidpc90@users.noreply.github.com>
2023-03-26 13:36:30 -0700

    half test cases pass

---
+++ dapp/contracts/StoryBet.sol
@@ -0,0 +1,182 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract StoryBet {
+
+    struct Story {
+        address ownerAddress;
+        uint numVote;
+        string[] tags;
+        uint256 publishedDateTime;
+        string storyText;
+        string[] comments;
+        bool exist;
+    }
+
+    mapping(address => Story) public userStory;
+    mapping(address => uint256) public userVoteBalance;
+
+    address owner;
+    address[] public users;
+    Story[] stories;
+
+    uint public votePrice;
+
+    constructor() payable{
+        owner = msg.sender;
+        votePrice = 100000000000000000; // 0.1 ether
+        userVoteBalance[msg.sender] = msg.value;
+    }
+
+
+
+
+    function createStory(string[] memory _tags, uint256 _publishedDateTime, string memory _storyText) public payable {
+        require(userVoteBalance[msg.sender] >= votePrice, "Not enough token to public a story!");
+        string[] memory comments;
+        Story memory newStory = Story(msg.sender, 0, _tags, _publishedDateTime, _storyText, comments, true);
+        userStory[msg.sender] = newStory;
+        stories.push(newStory);
+    }
+
+    function removeStory() public {
+        // uint256 l = stories.length;
+        // for (uint i = 0; i < l; i++){
+        //     if(stories[i].ownerAddress == msg.sender && stories[i].ownerAddress == msg.sender){
+        //         stories[i] = stories[l - 1];
+        //     }
+        // }
+        // stories.pop();
+
+        userStory[msg.sender].exist = false;
+        
+        uint256 l = stories.length;
+        for (uint i = 0; i < l; i++){
+            if(stories[i].ownerAddress == msg.sender){
+                stories[i].exist = false;
+            }
+        }
+    }
+
+    function buyVote(uint256 _buyVoteNum) public payable {
+        users.push(msg.sender);
+        require(userVoteBalance[msg.sender] >= _buyVoteNum * votePrice, "insufficient tokens.");
+        userVoteBalance[msg.sender] += _buyVoteNum;
+    }
+
+    function refundVote(uint256 _refundVoteNum) public payable {
+        require(userVoteBalance[msg.sender] >= _refundVoteNum, "insufficient votes remained in the account");
+        userVoteBalance[msg.sender] -= _refundVoteNum;
+        payable(msg.sender).transfer(votePrice * _refundVoteNum);
+    }
+
+    function vote(uint numVote1, address storyOwner) public payable{
+        require(numVote1 <= userVoteBalance[msg.sender], "ensure adequate account balances");
+        userVoteBalance[msg.sender] -= numVote1;
+        userStory[storyOwner].numVote += numVote1;
+        uint256 l = stories.length;
+        
+        for (uint i = 0; i < l; i++){
+            if(stories[i].ownerAddress == storyOwner){
+                stories[i].numVote += numVote1;
+            }
+        }
+        
+        // rankStories();
+    }
+
+    function summaryVotes() public payable {
+        require(msg.sender == owner, "only the admin can end vote");
+        Story[] memory result = rankStories();
+        // the winning dog
+        Story memory winningStory = result[0];
+        
+        address winner = winningStory.ownerAddress;
+
+        payable(winner).transfer((address(this).balance));
+        //payable(owner).transfer(address(this).balance);//transfer money back to owner
+        
+        clearBoard();
+    }
+
+    function rankStories() public returns (Story[] memory) {
+        Story[] memory newstory;
+        uint j = 0;
+        for(uint i ; i < stories.length; i++){
+            if(stories[i].exist == true){
+                newstory[j++] = stories[i];
+            }
+        }
+        sort(newstory);
+        return newstory;
+    }
+
+    function sort(Story[] memory data) public returns (Story[] memory) {
+        quickSort(data, int(0), int(data.length - 1));
+        return data;
+    }
+
+    function quickSort(Story[] memory arr, int left, int right) public{
+        int i = left;
+        int j = right;
+        if (i == j) return;
+        uint pivot = arr[uint(left + (right - left) / 2)].numVote;
+        while (i <= j) {
+            while (arr[uint(i)].numVote < pivot) i++;
+            while (pivot < arr[uint(j)].numVote) j--;
+            if (i <= j) {
+                (arr[uint(i)], arr[uint(j)]) = (arr[uint(j)], arr[uint(i)]);
+                i++;
+                j--;
+            }
+        }
+        if (left < j)
+            quickSort(arr, left, j);
+        if (i < right)
+            quickSort(arr, i, right);
+    }
+
+    function clearBoard() public {
+        uint len1 = stories.length;
+        uint len2 = users.length;
+        for(uint i = 0; i < len1; i++){
+            stories[i].exist = false;
+        }
+        
+        for(uint i = 0; i < len2; i++){
+            userStory[users[i]].exist = false;
+        }
+    }
+
+    function comment(address storyOwner, string memory storyText) public {
+        userStory[storyOwner].comments.push(storyText);
+    }
+
+    function getStories() public view returns (Story[] memory) {
+        return stories;
+    }
+
+    function getStoryByIndex(uint index) public view returns(Story memory) {
+        return stories[index];
+    }
+
+    function getUserVote(address addr) public view returns (uint256) {
+        return userVoteBalance[addr];
+    }
+
+    function getUserStoryVote(address addr) public view returns (uint256) {
+        return userStory[addr].numVote;
+    }
+
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+
+    function getAdmin() public view returns (address admin) {
+        return owner;
+    }
+
+    function getExist(address addr) public view returns (bool) {
+        return userStory[addr].exist;
+    }
+}
--- dapp/test/Lock.js
+++ dapp/test/Lock.js
@@ -1,126 +1,391 @@
-const {
-  time,
-  loadFixture,
-} = require("@nomicfoundation/hardhat-network-helpers");
-const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
-describe("Lock", function () {
-  // We define a fixture to reuse the same setup in every test.
-  // We use loadFixture to run this setup once, snapshot that state,
-  // and reset Hardhat Network to that snapshot in every test.
-  async function deployOneYearLockFixture() {
-    const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-    const ONE_GWEI = 1_000_000_000;
+describe("StoryBet", function() {
+  it("1. User have enough money to publish story", async function() {
+    owners = await ethers.getSigners();
+    StoryBet = await ethers.getContractFactory("StoryBet");
+    storyBet = await StoryBet.connect(owners[0]).deploy({value: "1000000000000000000"});
 
-    const lockedAmount = ONE_GWEI;
-    const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
-
-    // Contracts are deployed using the first signer/account by default
-    const [owner, otherAccount] = await ethers.getSigners();
-
-    const Lock = await ethers.getContractFactory("Lock");
-    const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-    return { lock, unlockTime, lockedAmount, owner, otherAccount };
-  }
-
-  describe("Deployment", function () {
-    it("Should set the right unlockTime", async function () {
-      const { lock, unlockTime } = await loadFixture(deployOneYearLockFixture);
-
-      expect(await lock.unlockTime()).to.equal(unlockTime);
-    });
-
-    it("Should set the right owner", async function () {
-      const { lock, owner } = await loadFixture(deployOneYearLockFixture);
-
-      expect(await lock.owner()).to.equal(owner.address);
-    });
-
-    it("Should receive and store the funds to lock", async function () {
-      const { lock, lockedAmount } = await loadFixture(
-        deployOneYearLockFixture
-      );
-
-      expect(await ethers.provider.getBalance(lock.address)).to.equal(
-        lockedAmount
-      );
-    });
-
-    it("Should fail if the unlockTime is not in the future", async function () {
-      // We don't use the fixture here because we want a different deployment
-      const latestTime = await time.latest();
-      const Lock = await ethers.getContractFactory("Lock");
-      await expect(Lock.deploy(latestTime, { value: 1 })).to.be.revertedWith(
-        "Unlock time should be in the future"
-      );
-    });
+    const tags = ["tag1", "tag2", "tag3"];
+    const publishedDateTime = 123456789;
+    const storyText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+    
+    await storyBet.createStory(tags, publishedDateTime, storyText);
   });
 
-  describe("Withdrawals", function () {
-    describe("Validations", function () {
-      it("Should revert with the right error if called too soon", async function () {
-        const { lock } = await loadFixture(deployOneYearLockFixture);
 
-        await expect(lock.withdraw()).to.be.revertedWith(
-          "You can't withdraw yet"
-        );
-      });
+  it("2. User do not have enough money to publish story", async function() {
+    owners = await ethers.getSigners();
+    StoryBet = await ethers.getContractFactory("StoryBet");
+    storyBet = await StoryBet.connect(owners[0]).deploy({value: "100"});
 
-      it("Should revert with the right error if called from another account", async function () {
-        const { lock, unlockTime, otherAccount } = await loadFixture(
-          deployOneYearLockFixture
-        );
+    const tags = ["tag1", "tag2", "tag3"];
+    const publishedDateTime = 123456789;
+    const storyText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+    
+    haveError = false;
 
-        // We can increase the time in Hardhat Network
-        await time.increaseTo(unlockTime);
+    try{
+      await storyBet.createStory(tags, publishedDateTime, storyText);
+      haveError = true;
+    }
+    catch(err){
+    }
 
-        // We use lock.connect() to send a transaction from another account
-        await expect(lock.connect(otherAccount).withdraw()).to.be.revertedWith(
-          "You aren't the owner"
-        );
-      });
-
-      it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
-        const { lock, unlockTime } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        // Transactions are sent using the first signer by default
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw()).not.to.be.reverted;
-      });
-    });
-
-    describe("Events", function () {
-      it("Should emit an event on withdrawals", async function () {
-        const { lock, unlockTime, lockedAmount } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw())
-          .to.emit(lock, "Withdrawal")
-          .withArgs(lockedAmount, anyValue); // We accept any value as `when` arg
-      });
-    });
-
-    describe("Transfers", function () {
-      it("Should transfer the funds to the owner", async function () {
-        const { lock, unlockTime, lockedAmount, owner } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw()).to.changeEtherBalances(
-          [owner, lock],
-          [lockedAmount, -lockedAmount]
-        );
-      });
-    });
+    if(haveError == true){
+      expect.fail();
+    }
   });
+
+
+  it("3. Multiple users create story", async function() {
+    users = await ethers.getSigners();
+    StoryBet = await ethers.getContractFactory("StoryBet");
+    storyBet_1 = await StoryBet.connect(owners[0]).deploy({value: "100000000000000000000"});
+    storyBet_2 = await StoryBet.connect(owners[1]).deploy({value: "100"});
+
+    const tags = ["tag1", "tag2", "tag3"];
+    const publishedDateTime = 123456789;
+    const storyText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+    
+
+    await storyBet_1.createStory(tags, publishedDateTime, storyText);
+
+
+    haveError = false;
+
+    try{
+      await storyBet_2.createStory(tags, publishedDateTime, storyText);
+      haveError = true;
+    }
+    catch(err){
+    }
+
+    if(haveError == true){
+      expect.fail();
+    }
+  });
+
+  it("4. User remove story", async function() {
+    owners = await ethers.getSigners();
+    StoryBet = await ethers.getContractFactory("StoryBet");
+    storyBet = await StoryBet.connect(owners[0]).deploy({value: "1000000000000000000"});
+
+    const tags = ["tag1", "tag2", "tag3"];
+    const publishedDateTime = 123456789;
+    const storyText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+    
+    await storyBet.createStory(tags, publishedDateTime, storyText);
+    
+    exist = await storyBet.getUserStoryExist(owners[0].address);
+    expect(exist).to.equal(true);
+    
+    await storyBet.removeStory();
+
+    exist = await storyBet.getUserStoryExist(owners[0].address);
+    expect(exist).to.equal(false);
+  });
+
+  it("5. User vote himself", async function() {
+    owners = await ethers.getSigners();
+    StoryBet = await ethers.getContractFactory("StoryBet");
+    storyBet = await StoryBet.connect(owners[0]).deploy({value: "10000000000000000000"});
+
+    const tags = ["tag1", "tag2", "tag3"];
+    const publishedDateTime = 123456789;
+    const storyText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+
+    haveError = false;
+    await storyBet.createStory(tags, publishedDateTime, storyText);
+
+    num_vote = await storyBet.getUserStoryVote(owners[0].address);
+    await storyBet.vote(1, owners[0].address);
+    new_vote = await storyBet.getUserStoryVote(owners[0].address);
+    expect(new_vote).to.equal(num_vote+1)
+
+  });
+
+
+
+
+  it("6. User vote for others test", async function() {
+    // owners = await ethers.getSigners();
+    // StoryBet = await ethers.getContractFactory("StoryBet");
+    // storyBet_1 = await StoryBet.connect(owners[0]).deploy({value: "50000000000000000000"});
+    // storyBet_2 = await StoryBet.connect(owners[1]).deploy({value: "50000000000000000000"});
+
+    // const tags = ["tag1", "tag2", "tag3"];
+    // const publishedDateTime = 123456789;
+    // const storyText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+    
+    // await storyBet_1.connect(owners[0]).createStory(tags, publishedDateTime, storyText);
+    // await storyBet_2.connect(owners[1]).createStory(tags, publishedDateTime, storyText);
+    
+    // oldnumvote = await storyBet_1.connect(owners[0]).getUserStoryVote(owners[1].address);
+
+    // await storyBet_1.connect(owners[0]).vote(1, owners[1].address);
+
+    // newnumvote = await storyBet_1.connect(owners[0]).getUserStoryVote(owners[1].address);
+    // expect(newnumvote).to.equal(oldnumvote+1);
+
+
+
+
+
+
+    owners = await ethers.getSigners();
+    StoryBet = await ethers.getContractFactory("StoryBet");
+    storyBet_1 = await StoryBet.connect(owners[0]).deploy({value: "50000000000000000000"});
+    storyBet_2 = await StoryBet.connect(owners[1]).deploy({value: "50000000000000000000"});
+
+    const tags = ["tag1", "tag2", "tag3"];
+    const publishedDateTime = 123456789;
+    const storyText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+    
+    await storyBet_1.createStory(tags, publishedDateTime, storyText);
+    await storyBet_2.createStory(tags, publishedDateTime, storyText);
+    
+    oldnumvote = await storyBet_1.getUserStoryVote(owners[1].address);
+
+    await storyBet_1.vote(1, owners[0].address, {value:"200000000000000000",from: owners[1].address});
+
+    newnumvote = await storyBet_1.getUserStoryVote(owners[1].address);
+    expect(newnumvote).to.equal(oldnumvote+1);
+
+    
+  });
+
+  it("7. User buy vote ", async function() {
+    owners = await ethers.getSigners();
+    StoryBet = await ethers.getContractFactory("StoryBet");
+    storyBet = await StoryBet.connect(owners[0]).deploy({value: '10000000000000000000'});
+
+    const tags = ["tag1", "tag2", "tag3"];
+    const publishedDateTime = 123456789;
+    const storyText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+
+    haveError = false;
+    await storyBet.createStory(tags, publishedDateTime, storyText);
+
+    old_balance = await storyBet.getUserVote(owners[0].address);
+    await storyBet.buyVote(1);
+    new_balance = await storyBet.getUserVote(owners[0].address);
+    expect(old_balance).to.equal(ethers.BigNumber.from('10000000000000000000'))
+
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // it("6. rankStories test", async function() {
+  //   owners = await ethers.getSigners();
+  //   StoryBet = await ethers.getContractFactory("StoryBet");
+  //   storyBet_0 = await StoryBet.connect(owners[0]).deploy({value: "200000000000000000000"});
+  //   storyBet_1 = await StoryBet.connect(owners[1]).deploy({value: "200000000000000000000"});
+  //   storyBet_2 = await StoryBet.connect(owners[2]).deploy({value: "200000000000000000000"});
+  //   storyBet_3 = await StoryBet.connect(owners[3]).deploy({value: "200000000000000000000"});
+
+  //   const tags = ["tag1", "tag2", "tag3"];
+  //   const publishedDateTime = 123456789;
+  //   const storyText0 = "story0.";
+  //   const storyText1 = "story1.";
+  //   const storyText2 = "story2.";
+  //   const storyText3 = "story3.";
+    
+  //   await storyBet_0.connect(owners[0]).createStory(tags, publishedDateTime, storyText0);
+  //   await storyBet_1.connect(owners[1]).createStory(tags, publishedDateTime, storyText1);
+  //   await storyBet_2.connect(owners[2]).createStory(tags, publishedDateTime, storyText2);
+  //   await storyBet_3.connect(owners[3]).createStory(tags, publishedDateTime, storyText3);
+
+  //   await storyBet_0.connect(owners[0]).vote(1, owners[1]);
+  //   await storyBet_1.vote(1, owners[1]);
+  //   await storyBet_2.vote(1, owners[2]);
+  //   await storyBet_3.vote(1, owners[1]);
+  //  // const vote = storyBet_0.getUserStoryVote(owners[0]);
+  // // expect(vote).to.equal(ethers.BigNumber.from("0"))
+
+  // });
+
+
+
+
+
+
+
+  // it("should remove a user's story", async function() {
+  //   const tags = ["tag1", "tag2", "tag3"];
+  //   const publishedDateTime = 123456789;
+  //   const storyText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+  //   const votePrice = await storyBet.votePrice();
+
+  //   // User 1 creates a new story
+  //   await expect(storyBet.connect(user1).createStory(tags, publishedDateTime, storyText, { value: votePrice }))
+  //     .to.emit(storyBet, "StoryCreated")
+  //     .withArgs(user1.address);
+
+  //   // User 1 removes their story
+  //   await expect(storyBet.connect(user1).removeStory())
+  //     .to.emit(storyBet, "StoryRemoved")
+  //     .withArgs(user1.address);
+
+  //   // Check that the user's story was removed correctly
+  //   const user1Story = await storyBet.userStory(user1.address);
+  //   expect(user1Story.exist).to.equal(false);
+  // });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const {time, loadFixture,} = require("@nomicfoundation/hardhat-network-helpers");
+// const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
+// const { expect } = require("chai");
+
+// describe("Lock", function () {
+//   // We define a fixture to reuse the same setup in every test.
+//   // We use loadFixture to run this setup once, snapshot that state,
+//   // and reset Hardhat Network to that snapshot in every test.
+//   async function deployOneYearLockFixture() {
+//     const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
+//     const ONE_GWEI = 1_000_000_000;
+
+//     const lockedAmount = ONE_GWEI;
+//     const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
+
+//     // Contracts are deployed using the first signer/account by default
+//     const [owner, otherAccount] = await ethers.getSigners();
+
+//     const Lock = await ethers.getContractFactory("Lock");
+//     const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+
+//     return { lock, unlockTime, lockedAmount, owner, otherAccount };
+//   }
+
+//   describe("Deployment", function () {
+//     it("Should set the right unlockTime", async function () {
+//       const { lock, unlockTime } = await loadFixture(deployOneYearLockFixture);
+
+//       expect(await lock.unlockTime()).to.equal(unlockTime);
+//     });
+
+//     it("Should set the right owner", async function () {
+//       const { lock, owner } = await loadFixture(deployOneYearLockFixture);
+
+//       expect(await lock.owner()).to.equal(owner.address);
+//     });
+
+//     it("Should receive and store the funds to lock", async function () {
+//       const { lock, lockedAmount } = await loadFixture(
+//         deployOneYearLockFixture
+//       );
+
+//       expect(await ethers.provider.getBalance(lock.address)).to.equal(
+//         lockedAmount
+//       );
+//     });
+
+//     it("Should fail if the unlockTime is not in the future", async function () {
+//       // We don't use the fixture here because we want a different deployment
+//       const latestTime = await time.latest();
+//       const Lock = await ethers.getContractFactory("Lock");
+//       await expect(Lock.deploy(latestTime, { value: 1 })).to.be.revertedWith(
+//         "Unlock time should be in the future"
+//       );
+//     });
+//   });
+
+//   describe("Withdrawals", function () {
+//     describe("Validations", function () {
+//       it("Should revert with the right error if called too soon", async function () {
+//         const { lock } = await loadFixture(deployOneYearLockFixture);
+
+//         await expect(lock.withdraw()).to.be.revertedWith(
+//           "You can't withdraw yet"
+//         );
+//       });
+
+//       it("Should revert with the right error if called from another account", async function () {
+//         const { lock, unlockTime, otherAccount } = await loadFixture(
+//           deployOneYearLockFixture
+//         );
+
+//         // We can increase the time in Hardhat Network
+//         await time.increaseTo(unlockTime);
+
+//         // We use lock.connect() to send a transaction from another account
+//         await expect(lock.connect(otherAccount).withdraw()).to.be.revertedWith(
+//           "You aren't the owner"
+//         );
+//       });
+
+//       it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
+//         const { lock, unlockTime } = await loadFixture(
+//           deployOneYearLockFixture
+//         );
+
+//         // Transactions are sent using the first signer by default
+//         await time.increaseTo(unlockTime);
+
+//         await expect(lock.withdraw()).not.to.be.reverted;
+//       });
+//     });
+
+//     describe("Events", function () {
+//       it("Should emit an event on withdrawals", async function () {
+//         const { lock, unlockTime, lockedAmount } = await loadFixture(
+//           deployOneYearLockFixture
+//         );
+
+//         await time.increaseTo(unlockTime);
+
+//         await expect(lock.withdraw())
+//           .to.emit(lock, "Withdrawal")
+//           .withArgs(lockedAmount, anyValue); // We accept any value as `when` arg
+//       });
+//     });
+
+//     describe("Transfers", function () {
+//       it("Should transfer the funds to the owner", async function () {
+//         const { lock, unlockTime, lockedAmount, owner } = await loadFixture(
+//           deployOneYearLockFixture
+//         );
+
+//         await time.increaseTo(unlockTime);
+
+//         await expect(lock.withdraw()).to.changeEtherBalances(
+//           [owner, lock],
+//           [lockedAmount, -lockedAmount]
+//         );
+//       });
+//     });
+//   });
+// });

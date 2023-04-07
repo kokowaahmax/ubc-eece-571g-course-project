84c21d343cb3c934c27b953f8ba8db1c4da33943
Mingrui Li <118086827+Davidpc90@users.noreply.github.com>
2023-04-07 15:54:54 -0700

    add story title to card

--- dapp/contracts/StoryBet.sol
+++ dapp/contracts/StoryBet.sol
@@ -7,6 +7,7 @@ contract StoryBet {
         address ownerAddress;
         uint numVote;
         string[] tags;
+        // string[] storyTitle;
         uint256 publishedDateTime;
         string storyText;
         string[] comments;
